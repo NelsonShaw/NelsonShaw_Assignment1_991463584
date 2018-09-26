6c51e05f3b9ac5392497ab50202cd8d77eeca82b
Nelson Shaw <shawne@sheridancollege.ca>
2018-09-26 12:20:19 -0400

    Added HomeController.cs. Added GuestResponse.cs. Added the Index home page and RsvpForm page. Created a strongly types RsvpForm page and added several entry boxes to allow user input.

---
+++ NelsonShaw_Assignment1_991463584/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NelsonShaw_Assignment1_991463584.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int time = DateTime.Now.Hour;
+            ViewBag.Greeting = time < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+    }
+}
