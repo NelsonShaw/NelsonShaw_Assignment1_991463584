dd23edd0d0aa1aa27dacfbd07cdf30f992308656
Nelson Shaw <shawne@sheridancollege.ca>
2018-09-29 12:28:56 -0400

    Added Restaurant Response class to represent a response for which restaurant was selected. Added Restaurant Thanks page for welcoming the customer to the restaurant. Modified Index Page in order to receive restaurant information. Modified home controller to add action methods to send restaurant information over.

--- NelsonShaw_Assignment1_991463584/Controllers/HomeController.cs
+++ NelsonShaw_Assignment1_991463584/Controllers/HomeController.cs
@@ -9,21 +9,36 @@ namespace NelsonShaw_Assignment1_991463584.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index()
+        public ActionResult Index()
         {
             int time = DateTime.Now.Hour;
             ViewBag.Greeting = time < 12 ? "Good Morning" : "Good Afternoon";
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Index(RestaurantResponse restaurantResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Email response to the party organizer
+                return View("RestaurantThanks", restaurantResponse);
+            }
+            else
+            {
+                // there is a validation error
+                return View();
+            }
+        }
+
         [HttpGet]
-        public ViewResult RsvpForm()
+        public ActionResult RsvpForm()
         {
             return View();
         }
 
         [HttpPost]
-        public ViewResult RsvpForm(GuestResponse guestResponse)
+        public ActionResult RsvpForm(GuestResponse guestResponse)
         {
             if (ModelState.IsValid)
             {
---
+++ NelsonShaw_Assignment1_991463584/Models/RestaurantResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace NelsonShaw_Assignment1_991463584.Models
+{
+    public class RestaurantResponse
+    {
+        [Required(ErrorMessage = "Please select a restaurant")]
+        public string RestaurantName { get; set; }
+    }
+}
